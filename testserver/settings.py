cbb17b518bb6d1ee512376a89ca6c0e6c10d1711
auto007 <puk668@foxmail.com>
2018-09-17 16:58:43 +0800

    允许所有ip访问

--- testserver/settings.py
+++ testserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#11)st1a%^^!p1oac$ldoa0&xcc4p9d$)_cwsw-^6e47=%jl8m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
